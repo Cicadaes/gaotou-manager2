707dc99ec5e63071cd3bdc9c5235688156b89b0b
Moonshine <710159940@qq.com>
2018-12-21 16:43:26 +0800

    1、系统配置下的事件类型，数据修改(修改)

--- src/app/business/system/event-type/event-type.component.ts
+++ src/app/business/system/event-type/event-type.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ConfirmationService, Message, MessageService} from 'primeng/api';
 import {GlobalService} from '../../../common/services/global.service';
 import {SystemService} from '../../../common/services/system.service';
-import {AddEventType, EventType} from '../../../common/model/system-model';
+import {AddEventType, EventType, ModifyEventType} from '../../../common/model/system-model';
 
 @Component({
   selector: 'app-event-type',
@@ -18,6 +18,10 @@ export class EventTypeComponent implements OnInit {
   // 增加相关
   public addDialog: boolean; // 增加弹窗显示控制
   public addEventType: AddEventType = new AddEventType();
+  // 修改相关
+  public modifyDialog: boolean; // 修改弹窗显示控制
+  public modifyEventType: ModifyEventType = new ModifyEventType();
+  // public modifyList: any[]; // 选择的数据
   // 其他提示弹窗相关
   public cleanTimer: any; // 清除时钟
   public msgs: Message[] = []; // 消息弹窗
@@ -26,7 +30,8 @@ export class EventTypeComponent implements OnInit {
     private messageService: MessageService,
     private confirmationService: ConfirmationService,
     private globalService: GlobalService
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     this.cols = [
@@ -36,6 +41,7 @@ export class EventTypeComponent implements OnInit {
     ];
     this.updateEventTypeDate();
   }
+
   public updateEventTypeDate(): void {
     this.systemService.searchEventTypeList({page: 1, nums: 1000}).subscribe(
       (value) => {
@@ -44,11 +50,14 @@ export class EventTypeComponent implements OnInit {
       }
     );
   }
+
   // 选中后赋值
   public onRowSelect(event): void {
     console.log(event.data);
     this.eventType = this.cloneCar(event.data);
+    // this.modifyList = event.data;
   }
+
   // 遍历修改后的数据，并把它赋值给car1
   public cloneCar(c: any): any {
     const car = {};
@@ -59,14 +68,15 @@ export class EventTypeComponent implements OnInit {
     }
     return car;
   }
+
   // 增加
   public addsSave(): void {
     console.log(this.addEventType);
-   /* if (this.addVideo.inStore === '1') {
-      this.addVideo.inStore = true;
-    } else {
-      this.addVideo.inStore = false;
-    }*/
+    /* if (this.addVideo.inStore === '1') {
+       this.addVideo.inStore = true;
+     } else {
+       this.addVideo.inStore = false;
+     }*/
     this.confirmationService.confirm({
       message: `确定要增加吗？`,
       header: '增加提醒',
@@ -117,21 +127,25 @@ export class EventTypeComponent implements OnInit {
           }
         );
       },
-      reject: () => {}
+      reject: () => {
+      }
     });
   }
+
   // 删除
   public deleteFirm(): void {
     if (this.selecteEventTypes === undefined || this.selecteEventTypes.length === 0) {
       if (this.cleanTimer) {
         clearTimeout(this.cleanTimer);
       }
+      // 判断是否选择
       this.msgs = [];
       this.msgs.push({severity: 'error', summary: '操作错误', detail: '请选择需要删除的项'});
       this.cleanTimer = setTimeout(() => {
         this.msgs = [];
       }, 3000);
     } else {
+      // 删除单个
       this.confirmationService.confirm({
         message: `确定要删除这${this.selecteEventTypes.length}项吗？`,
         header: '删除提醒',
@@ -182,8 +196,9 @@ export class EventTypeComponent implements OnInit {
               }
             );
           } else {
+            // 删除多个
             const ids = [];
-            for (let i = 0; i < this.selecteEventTypes.length; i ++) {
+            for (let i = 0; i < this.selecteEventTypes.length; i++) {
               ids.push(this.selecteEventTypes[i].id);
             }
             this.systemService.deleteEventTypeList(ids).subscribe(
@@ -230,8 +245,101 @@ export class EventTypeComponent implements OnInit {
             );
           }
         },
-        reject: () => {}
+        reject: () => {
+        }
       });
     }
+  }
+
+  // 修改
+  public modifyBtn(): void {
+    // if (this.)
+    // console.log(this.selecteEventTypes[0]['id']);
+    // if (this.selecteEventTypes.length > 1) {
+    // }
+    if (this.selecteEventTypes === undefined || this.selecteEventTypes.length === 0) {
+      if (this.cleanTimer) {
+        clearTimeout(this.cleanTimer);
+      }
+      this.msgs = [];
+      this.msgs.push({severity: 'error', summary: '操作错误', detail: '请选择需要修改的项'});
+      this.cleanTimer = setTimeout(() => {
+        this.msgs = [];
+      }, 3000);
+    } else if (this.selecteEventTypes.length > 0 && this.selecteEventTypes.length <= 1) {
+      this.modifyDialog = true;
+      this.modifyEventType.categoryCode = this.selecteEventTypes[0]['categoryCode'];
+      this.modifyEventType.eventCategoryName = this.selecteEventTypes[0]['eventCategoryName'];
+    } else {
+      if (this.cleanTimer) {
+        clearTimeout(this.cleanTimer);
+      }
+      this.msgs = [];
+      this.msgs.push({severity: 'error', summary: '操作错误', detail: '最多选择一项修改'});
+      this.cleanTimer = setTimeout(() => {
+        this.msgs = [];
+      }, 3000);
+    }
+  }
+
+  // 修改确认
+  public modifyData(): void {
+    this.confirmationService.confirm({
+      message: `确定要修改吗？`,
+      header: '修改提醒',
+      icon: 'pi pi-exclamation-triangle',
+      accept: () => {
+        this.modifyEventType.id = this.selecteEventTypes[0].id;
+        this.modifyEventType.idt = this.selecteEventTypes[0].idt;
+        console.log(this.modifyEventType);
+        this.globalService.eventSubject.next({display: true});
+        this.systemService.modifyEventTypeItem(this.modifyEventType).subscribe(
+          (value) => {
+            if (value.status === '200') {
+              this.globalService.eventSubject.next({display: false});
+              if (this.cleanTimer) {
+                clearTimeout(this.cleanTimer);
+              }
+              this.msgs = [];
+              this.selecteEventTypes = undefined;
+              this.msgs.push({severity: 'success', summary: '修改提醒', detail: value.message});
+              this.updateEventTypeDate();
+              this.cleanTimer = setTimeout(() => {
+                this.msgs = [];
+              }, 3000);
+              this.modifyDialog = false;
+            } else {
+              setTimeout(() => {
+                this.globalService.eventSubject.next({display: false});
+                if (this.cleanTimer) {
+                  clearTimeout(this.cleanTimer);
+                }
+                this.msgs = [];
+                this.msgs.push({severity: 'error', summary: '增加提醒', detail: '服务器处理失败'});
+                this.cleanTimer = setTimeout(() => {
+                  this.msgs = [];
+                }, 3000);
+              }, 3000);
+            }
+          },
+          (err) => {
+            console.log(err);
+            setTimeout(() => {
+              this.globalService.eventSubject.next({display: false});
+              if (this.cleanTimer) {
+                clearTimeout(this.cleanTimer);
+              }
+              this.msgs = [];
+              this.msgs.push({severity: 'error', summary: '增加提醒', detail: '连接服务器失败'});
+              this.cleanTimer = setTimeout(() => {
+                this.msgs = [];
+              }, 3000);
+            }, 3000);
+          }
+        );
+      },
+      reject: () => {
+      }
+    });
   }
 }
--- src/app/common/model/system-model.ts
+++ src/app/common/model/system-model.ts
@@ -14,11 +14,21 @@ export class EventType {
   idt: string; // 创建时间
   udt: string; // 更新时间
 }
+
 export class AddEventType {
   categoryCode: string; // 分类编码
   // enabled: any; // 是否启用
   eventCategoryName: string; // 事件分类名称
   // id: number; // 唯一id
   // idt: string; // 创建时间
+  // udt: string; // 更新时间
+}
+
+export class ModifyEventType {
+  categoryCode: string; // 分类编码
+  // enabled: any; // 是否启用
+  eventCategoryName: string; // 事件分类名称
+  id: number; // 唯一id
+  idt: string; // 创建时间
   // udt: string; // 更新时间
 }
